2f1278e44b303712ffd500cfc1fa8e95ae3c0c5e
Aaron Turner <synfinatic@gmail.com>
2022-02-25 19:39:49 -0800

    release 1.7.4

--- Formula/aws-sso-cli.rb
+++ Formula/aws-sso-cli.rb
@@ -1,22 +1,22 @@
 class AwsSsoCli < Formula
   desc "Securely manage AWS API credentials using AWS SSO"
   homepage "https://github.com/synfinatic/aws-sso-cli"
-  url "https://github.com/synfinatic/aws-sso-cli/archive/refs/tags/v1.7.3.tar.gz"
-  sha256 "086e5ace47060fc4129ebd034f6108d39b78f68968b39f28a8c8105ea4427066"
+  url "https://github.com/synfinatic/aws-sso-cli/archive/refs/tags/v1.7.4.tar.gz"
+  sha256 "6c017bb23dec1e18c71ec69b9c3c7be6776755dddaef87fc37cf4c0acf65da58"
 
   depends_on :xcode
   depends_on "go" => [:build, "1.17.0"]
 
   def install
     ENV["BREW_INSTALL"] = "1"
-    ENV["PROJECT_COMMIT"] = "9520f708c985cc8015e2d0b2f7781261ef5f1664"
+    ENV["PROJECT_COMMIT"] = "465eaafbcb29ea217e587a592ec5692937a3f176"
 
     system "make"
     bin.install "./dist/aws-sso"
   end
 
   test do
-    assert_match /AWS SSO CLI Version 1.7.3/, shell_output("#{bin}/aws-sso version")
+    assert_match /AWS SSO CLI Version 1.7.4/, shell_output("#{bin}/aws-sso version")
     assert_match /Please specify --sso/, shell_output("#{bin}/aws-sso --config /dev/null  2>&1")
   end
 end
