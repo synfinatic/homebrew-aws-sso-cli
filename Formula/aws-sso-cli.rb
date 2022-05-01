8d13bfc50e74a9e4f8437e5f768a957f7bdd8390
Aaron Turner <synfinatic@gmail.com>
2022-04-30 18:23:19 -0700

    re-release v1.8.0

--- Formula/aws-sso-cli.rb
+++ Formula/aws-sso-cli.rb
@@ -2,14 +2,14 @@ class AwsSsoCli < Formula
   desc "Securely manage AWS API credentials using AWS SSO"
   homepage "https://github.com/synfinatic/aws-sso-cli"
   url "https://github.com/synfinatic/aws-sso-cli/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "9b78384238261f9da7e8b1b9d17259629874c198d07dca2acac3dbf8d7333404"
+  sha256 "cdd9a5e8ad201f657ec8efa7822af3d238f4c6b071aeec0c91a93b7b3f1c2468"
 
   depends_on :xcode
   depends_on "go" => [:build, "1.17.0"]
 
   def install
     ENV["BREW_INSTALL"] = "1"
-    ENV["PROJECT_COMMIT"] = "0a910b1ed4f06cad97b4cafaadf1dbccb28eed74"
+    ENV["PROJECT_COMMIT"] = "be3ee7b8345d807e197eafee5ed9f2ac31312d95"
 
     system "make"
     bin.install "./dist/aws-sso"
